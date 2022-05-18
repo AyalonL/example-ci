8b276bdfa47cb131d0eac75c93637a73bf9c71ff
AyaloL <ayalon@skylinerobotics.com>
2022-05-18 09:20:54 +0300

    fixin bug

--- design6.py
+++ design6.py
@@ -19,4 +19,4 @@ def test_fizzbuzz():
     assert fizzbuzz(3) == "Fizz"
     assert fizzbuzz(5) == "Buzz"
     assert fizzbuzz(15) == "FizzBuzz"
-    assert fizzbuzz(1) == 8
+    assert fizzbuzz(1) == 1
